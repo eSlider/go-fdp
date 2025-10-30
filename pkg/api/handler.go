f1b582e7751bd864495a43677e81671c8e7fcf29
Andriy Oblivantsev <eslider@gmail.com>
2025-10-30 21:57:59 +0000

    Expand `Frame` validation in `AssetRequest` to include `1s` timeframe.

--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -104,7 +104,7 @@ type AssetRequest struct {
 	Market     string `validate:"required"`
 	Exchange   string `validate:"omitempty"`
 	MarketType string `validate:"omitempty,oneof=spot futures options"`
-	Frame      string `validate:"omitempty,oneof=1m 3m 5m 15m 30m 1h 2h 4h 6h 8h 12h 1d 3d 1w 1M"`
+	Frame      string `validate:"omitempty,oneof=1s 1m 3m 5m 15m 30m 1h 2h 4h 6h 8h 12h 1d 3d 1w 1M"`
 	Indicator  string `validate:"omitempty,oneof=klines aggTrades"`
 }
 
